abf7218f5b3c817a805fd69e760511c3702e0fcf
AndresBojaca <andresbojaca742@gmail.com>
2025-01-17 04:13:52 -0500

    feature(ThemeSwitcher): Enhance theme switcher button with icons and updated styles

--- src/components/Theme/ThemeSwitcher.tsx
+++ src/components/Theme/ThemeSwitcher.tsx
@@ -2,6 +2,10 @@
 
 import React, { useEffect } from 'react';
 import { useTheme } from '../../context/ThemeContext';
+import {
+    SunIcon,
+    MoonIcon
+} from "@radix-ui/react-icons";
 
 const ThemeSwitcher: React.FC = () => {
     const { theme, toggleTheme } = useTheme();
@@ -12,7 +16,7 @@ const ThemeSwitcher: React.FC = () => {
 
     return (
         <div className='fixed bottom-10 w-auto right-10'>
-            <button type="button" onClick={toggleTheme} className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{theme}</button>
+            <button type="button" onClick={toggleTheme} className="text-dark bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm p-4 me-2 mb-2 dark:text-light dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{theme == 'light' ? <MoonIcon /> : <SunIcon /> }</button>
         </div>
     );
 };
