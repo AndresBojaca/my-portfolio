ed41c189f8d3d715bed026ecdc6b6999283d4904
AndresBojaca <andresbojaca742@gmail.com>
2025-01-23 00:08:05 -0500

    feat(ThemeSwitcher): Add construction notice and adjust z-index for visibility

--- src/components/Theme/ThemeSwitcher.tsx
+++ src/components/Theme/ThemeSwitcher.tsx
@@ -15,7 +15,8 @@ const ThemeSwitcher: React.FC = () => {
     }, [theme]);
 
     return (
-        <div className='fixed bottom-10 w-auto right-10'>
+        <div className='fixed bottom-10 w-auto right-10 z-50'>
+            <span className="bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-yellow-900 dark:text-yellow-300">Sitio web en Construcción!</span>
             <button type="button" onClick={toggleTheme} className="text-dark bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm p-4 me-2 mb-2 dark:text-light dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{theme == 'light' ? <MoonIcon /> : <SunIcon /> }</button>
         </div>
     );
