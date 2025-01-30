80241ce1e5439744fea670f5137ffc425ccea57f
AndresBojaca <andresbojaca742@gmail.com>
2025-01-30 01:05:03 -0500

    refactor(Hero, JobExperience, Projects): Improve layout and styling for better responsiveness and loading state handling

--- src/containers/Projects/Projects.tsx
+++ src/containers/Projects/Projects.tsx
@@ -3,43 +3,49 @@
 import React, { useEffect, useState } from "react";
 import type { Projects, GithubProject } from '../../app/libs/types';
 import ProjectCard from "../../components/pages/ProjectCard/ProjectCard";
-import {
-  BellIcon
-} from "@radix-ui/react-icons";
+import SkeletonProjectCard from "@/components/pages/ProjectCard/SkeletonProjectCard";
 import './Projects.css';
 
 //TODO #1. Poner loader
 //TODO #2. Poner mensaje de error
 
 const fetchGithubRepos = async () => {
-  const response = await fetch(`https://api.github.com/users/AndresBojaca/repos`)
-  return response.json();
+  try {
+    const response = await fetch(`https://api.github.com/users/AndresBojaca/repos`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  } catch (error) {
+    console.error('Fetch error:', error);
+    throw error;
+  }
 }
 
 const Projects = () => {
 
   const [features, setFeatures] = useState<Projects[]>([]);
-
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchGithubRepos()
       .then(data => {
         // Filter by topics
-        const topicsFilter = data.filter((project: { topics: string[], visibility: string }) => 
-          project.topics 
-          && 
-          project.topics.includes('portfolio') 
+        const topicsFilter = data.filter((project: { topics: string[], visibility: string }) =>
+          project.topics
+          &&
+          project.topics.includes('portfolio')
           && project.visibility === 'public')
-          .sort((a: { created_at: string }, b: { created_at: string }) => 
-          new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
-        );
+          .sort((a: { created_at: string }, b: { created_at: string }) =>
+            new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+          );
 
         // Map projects
         const projects = topicsFilter.map((project: GithubProject, index: number) => {
           const { id, name, html_url, description, topics, created_at } = project;
           return {
             id,
-            name:name,
+            name: name,
             description: description,
             href: html_url,
             tecnologies: topics,
@@ -47,24 +53,31 @@ const Projects = () => {
           }
         });
         setFeatures(projects);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching projects:', error);
+        setLoading(false);
+        // Handle error state here if needed
       });
   }, []);
 
   return (
-    <section className="min-h-[100vh] bg-black/[0.01]">
-    <div className="py-16 md:py-36 scroll-m-20 w-full mx-auto container lg:max-w-4xl md:max-w-1xl text-center md:text-left lg:text-left">
+    <section className="min-h-[100vh]">
+      <div className="py-16 md:py-36 scroll-m-20 flex flex-col justify-center align-middle w-full mx-auto container lg:max-w-4xl md:max-w-2xl">
         <h1 className="text-2xl font-bold">Proyectos</h1>
         <div className="mt-[4rem]">
-            <div className="grid grid-cols-2 gap-4">
-              {features.map((feature) => (
+          <div className="grid lg:grid-cols-2 md:grid-cols-1 grid-cols-1 gap-4">
+            {loading ? <SkeletonProjectCard quantity={6} /> : (
+              features.map((feature) => (
                 <ProjectCard key={feature.id} {...feature} />
-              ))}
-            </div>
+              ))
+            )}
+          </div>
         </div>
-    </div>
-</section>
+      </div>
+    </section>
   );
 }
-
 
 export default Projects;
