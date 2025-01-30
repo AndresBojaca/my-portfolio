2230123c55332526e5eab43fac80341fa447e47e
AndresBojaca <andresbojaca742@gmail.com>
2025-01-30 01:29:19 -0500

    feat(ProjectCard, JobExperience): Add homepage link to ProjectCard and improve layout in JobExperience

--- src/components/pages/ProjectCard/ProjectCard.tsx
+++ src/components/pages/ProjectCard/ProjectCard.tsx
@@ -4,7 +4,7 @@ import SkillPill from "../../ui/skillPill";
 import { GitHubLogoIcon, GlobeIcon } from "@radix-ui/react-icons";
 
 
-const ProjectCard = ({ id, name, description, href, tecnologies, date }: ProjectCard) => {
+const ProjectCard = ({ id, name, description, href, tecnologies, date, homepage }: ProjectCard) => {
 
   const placeHolder = `https://fakeimg.pl/600x400/f5f5f5/909090?text=</>&font=museo&font_size=90`;
   const [imageSrc, setImageSrc] = React.useState(placeHolder);
@@ -34,7 +34,7 @@ const ProjectCard = ({ id, name, description, href, tecnologies, date }: Project
         <div className="flex flex-col justify-between p-4 leading-normal gap-2">
           <div className="flex gap-4 flex-col md:flex-row-reverse lg:flex-row-reverse">
             <div className="flex-shrink-0 w-full h-40 md:h-20 md:w-20 lg:h-20 lg:w-20 bg-transparent rounded-lg overflow-hidden border-[5px] border-transparent dark:border-slate-900 shadow-[0_0_0_1px_rgba(0,0,0,0.25)] shadow-slate-200 dark:shadow-slate-600">
-              <img src={imageSrc} className="object-cover w-full h-full transition duration-300 scale-[1.05] hover:scale-[1]" loading="lazy" alt="" />
+              <img src={imageSrc} loading="lazy" className="object-cover w-full h-full transition duration-300 scale-[1.05] hover:scale-[1]" alt="" />
             </div>
             <div className="flex flex-col flex-grow gap-2">
               <h5 className="text-xl font-bold tracking-tight text-dark dark:text-light">{name}</h5>
@@ -51,7 +51,13 @@ const ProjectCard = ({ id, name, description, href, tecnologies, date }: Project
           </div>
           <div className="flex gap-1 border-t dark:border-gray-800 pt-4">
             <a href={href} target="_blank" className="flex gap-1 text-white dark:text-slate-800 bg-slate-800 dark:bg-slate-100 font-medium rounded-lg text-xs px-2.5 py-1.5 text-center">Código <GitHubLogoIcon /></a>
-            <a target="_blank" type="button" className="flex gap-1 text-white dark:text-slate-800 bg-slate-800 dark:bg-slate-100 font-medium rounded-lg text-xs px-2.5 py-1.5 text-center">Demo <GlobeIcon /></a>
+            {
+              homepage && homepage.trim() !== "" && (
+              <a href={homepage} target="_blank" type="button" className="flex gap-1 text-white dark:text-slate-800 bg-slate-800 dark:bg-slate-100 font-medium rounded-lg text-xs px-2.5 py-1.5 text-center">
+                Demo <GlobeIcon />
+              </a>
+              )
+            }
           </div>
         </div>
       </div>
--- src/containers/JobExperience/JobExperience.tsx
+++ src/containers/JobExperience/JobExperience.tsx
@@ -7,7 +7,7 @@ const JobExperience = () => {
 
   return (
     <section className="min-h-[100vh]">
-      <div className="py-16 md:py-36 scroll-m-20 w-full mx-auto lg:max-w-4xl md:max-w-2xl text-center md:text-left lg:text-left">
+      <div className="py-16 md:py-36 scroll-m-20 flex flex-col justify-center align-middle w-full mx-auto container lg:max-w-4xl md:max-w-2xl text-center md:text-left lg:text-left">
         <h1 className="text-2xl font-bold">Experiencia de Trabajo</h1>
         <div className="mt-[4rem]">
           <JobList />
